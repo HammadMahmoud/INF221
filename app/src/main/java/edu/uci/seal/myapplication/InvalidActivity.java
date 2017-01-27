b6928e13562ed54d0eada9cfa37059d0d3ac1818
Mahmoud Hammad <hammadm@ics.uci.edu>
2017-01-26 20:42:10 -0800

    Check if the app has the READ_PHONE_STATE permission

--- app/src/main/java/edu/uci/seal/myapplication/InvalidActivity.java
+++ app/src/main/java/edu/uci/seal/myapplication/InvalidActivity.java
@@ -1,14 +1,19 @@
 package edu.uci.seal.myapplication;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 public class InvalidActivity extends AppCompatActivity {
 
+    private static int REQUEST_CODE=100;
     private TextView invalidTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +29,21 @@ public class InvalidActivity extends AppCompatActivity {
             String username = receivedIntent.getStringExtra("U");
             String password = receivedIntent.getStringExtra("P");
             //obtain the device unique ID
-            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-            String deviceID = telephonyManager.getDeviceId();
-            //send an Intent with U, P, and DeviceID extra data
-            Intent serviceIntent = new Intent("A3");
-//            serviceIntent.setPackage(this.getPackageName());
-            serviceIntent.putExtra("U", username);
-            serviceIntent.putExtra("P", password);
-            serviceIntent.putExtra("DeviceID", deviceID);
-            startService(serviceIntent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE);
+            }else {
+
+                TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                String deviceID = telephonyManager.getDeviceId();
+                //send an Intent with U, P, and DeviceID extra data
+                Intent serviceIntent = new Intent("A3");
+                serviceIntent.setPackage(this.getPackageName());
+                serviceIntent.putExtra("U", username);
+                serviceIntent.putExtra("P", password);
+                serviceIntent.putExtra("DeviceID", deviceID);
+                startService(serviceIntent);
+            }
 
         }
 
